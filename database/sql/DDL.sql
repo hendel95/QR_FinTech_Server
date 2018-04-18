ca2b82504bcb74392d0414e1d29c2870fd1818fa
csh61033 <csh61033@naver.com>
2018-04-18 18:03:34 +0900

    user Table 추가

---
+++ database/sql/DDL.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA `dbg_db` ;
+
+CREATE TABLE `dbg_db`.`user` (
+  `num` INT NOT NULL AUTO_INCREMENT,
+  `id` VARCHAR(45) NOT NULL,
+  `pw` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `type` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`num`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `num_UNIQUE` (`num` ASC));
